c08fb7aeb64c983d587d82d0c5d8c966ab685344
winne1601 <97727497+winne1601@users.noreply.github.com>
2022-02-18 14:06:47 +0700

    Add files via upload

---
+++ ChuVi.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class ChuVi {
+public static void main(String[]args) {
+	Scanner sc= new Scanner(System.in);
+    double r=sc.nextDouble();
+    double pi=3.14;
+    System.out.println("Chu vi ="+(2*pi*r));
+}
+}
